c4e74bd619a575c5b34c74338e1fe4d531ed787d
sonaleshraddha1999@gmail.com <sonalshraddha1999@gmail.com>
2023-07-01 16:32:08 +0530

    first commit

---
+++ StringReverse1.java
@@ -0,0 +1,11 @@
+public class StringReverse1{
+public static void main(String[] args){
+StringBuilder A = new StringBuilder("Shraddha");
+StringBuilder B = new StringBuilder("ahddarhs");
+A.reverse();
+B.reverse();
+System.out.println(A);
+System.out.println(B);
+
+}
+}
