3c0e5b4453f11a3ff6a7c0fcf96153b8d638ee42
sonaleshraddha1999@gmail.com <sonalshraddha1999@gmail.com>
2023-07-01 21:16:52 +0530

    first commit

---
+++ SecondLargest.java
@@ -0,0 +1,8 @@
+public class SecondLargest{
+public static void main(String[] args){
+int[] Array = {20,40,80,};
+int size = Array.length;
+int abc = Array[size-2];
+System.out.println("Second Largest No is :- " +abc);
+}
+}
